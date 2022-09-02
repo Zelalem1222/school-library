a8f1067741dcbb2fbd552646f082cacfa8c1815f
Zelalem1222 <zelalemfissha394@gmail.com>
2022-09-02 10:24:18 +0300

    Modify teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, parent_permission, specialization, name = 'Unknown')
+  def initialize(age, name, parent_permission, specialization)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
